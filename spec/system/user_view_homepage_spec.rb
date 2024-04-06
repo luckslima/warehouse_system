7988a8d6cd53f72eae9f09403cbc78f4e280bf4e
Lucas Lima <limalucks@hotmail.com>
2024-04-06 12:19:13 -0300

    Criando um teste inicial e a estrutura home#index

---
+++ spec/system/user_view_homepage_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe 'Usuário visita tela inicial' do
+
+    it 'e vê o nome da app' do
+        # Arrange
+
+        #Act
+        visit('/')
+
+        #Assert
+        expect(page).to have_content('Galpões & Estoque')
+
+    end
+
+end
