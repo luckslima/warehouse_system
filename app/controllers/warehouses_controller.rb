5e455e7ba57e09bfaeaddb890dba79e6dcebe7ef
Lucas Lima <limalucks@hotmail.com>
2024-04-07 18:32:18 -0300

    Implementando validações iniciais.

--- app/controllers/warehouses_controller.rb
+++ app/controllers/warehouses_controller.rb
@@ -5,13 +5,18 @@ class WarehousesController < ApplicationController
     end
 
     def new
+        @warehouse = Warehouse.new()
     end
 
     def create
         warehouse_params = params.require(:warehouse).permit(:name, :code, :city, :description, :address, :cep, :area)
-        w = Warehouse.new(warehouse_params)
-        w.save()
+        @warehouse = Warehouse.new(warehouse_params)
+        if @warehouse.save()
+            redirect_to root_path, notice: 'Galpão cadastrado com sucesso!'
+        else
+            flash.now[:notice] = 'Galpão não cadastrado.'
+            render 'new'
+        end
         
-        redirect_to root_path, notice: 'Galpão cadastrado com sucesso!'
     end
 end
--- spec/system/user_view_homepage_spec.rb
+++ spec/system/user_view_homepage_spec.rb
@@ -15,8 +15,10 @@ describe 'Usuário visita tela inicial' do
 
     it 'e vê os galpões cadastrados' do
         # Arreange
-        Warehouse.create(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000)
-        Warehouse.create(name: 'Maceio', code: 'MCZ', city: 'Maceio', area: 50_000)
+        Warehouse.create(name: 'Rio', code: 'SDU', city: 'Rio de Janeiro', area: 60_000,
+                         address:'Av do porto, 1000' , cep:'20000-000', description:'Galpão do Rio')
+        Warehouse.create(name: 'Maceio', code: 'MCZ', city: 'Maceio', area: 50_000,
+                         address:'Av Atlantica, 50' , cep:'80000-000', description:'Perto do Aeroporto')
 
         #Act
         visit(root_path)
