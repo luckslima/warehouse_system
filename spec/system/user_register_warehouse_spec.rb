edbf52adee61170aa69ff9fe4efa4a8ac53906bb
Lucas Lima <limalucks@hotmail.com>
2024-04-09 18:55:09 -0300

    Implementação de internacionalização (i18n)

--- spec/system/user_register_warehouse_spec.rb
+++ spec/system/user_register_warehouse_spec.rb
@@ -31,7 +31,7 @@ describe 'usuario cadastra um galpão' do
         fill_in 'Cidade', with: 'Rio de Janeiro'
         fill_in 'CEP', with: '20100-000'
         fill_in 'Área', with: '32000'    
-        click_on 'Enviar'
+        click_on 'Criar Galpão'
 
         #Assert
         expect(current_path).to eq root_path
@@ -51,10 +51,14 @@ describe 'usuario cadastra um galpão' do
         fill_in 'Nome', with: ''
         fill_in 'Descrição', with: ''
         fill_in 'Código', with: ''
-        click_on 'Enviar'
+        click_on 'Criar Galpão'
 
         #Assert
         expect(page).to have_content 'Galpão não cadastrado.'
+        expect(page).to have_content 'Nome não pode ficar em branco'
+        expect(page).to have_content 'Descrição não pode ficar em branco'
+        expect(page).to have_content 'Código não pode ficar em branco'
+
 
     end
 end
