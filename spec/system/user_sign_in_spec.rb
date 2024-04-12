eb1b3320036c9b6c0ec0bd7ec0f27987c08554b5
Lucas Lima <limalucks@hotmail.com>
2024-04-12 09:48:10 -0300

    Implementado o Login de usuários com o devise.

---
+++ spec/system/user_sign_in_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe 'Usuário se autentica' do
+    it 'com sucesso' do
+        #Arrange 
+        User.create!(name: 'João', email: 'joao@email.com', password: 'password')
+
+        #Act
+        visit root_path
+        click_on 'Entrar'
+        fill_in 'E-mail', with: 'joao@email.com'
+        fill_in 'Senha', with: 'password'
+        within('form') do
+            click_on 'Entrar'
+        end
+
+
+        #Assert
+        expect(page).to have_link 'Sair'
+        expect(page).not_to have_link 'Entrar'
+        within('nav') do
+            expect(page).to have_content 'João'
+        end
+        expect(page).to have_content 'Login efetuado com sucesso.'
+
+
+    end
+end
